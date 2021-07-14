bfa4b520f8df7c67169e7a259e26e6ae96651959
akane10 <targaryen.akane@gmail.com>
2021-07-14 10:47:42 +0700

    sha

--- cron_warga_bantu_warga/src/main.rs
+++ cron_warga_bantu_warga/src/main.rs
@@ -1,21 +1,40 @@
 use reqwest::header::HeaderName;
 use serde::de::DeserializeOwned;
+use serde_json::map::Map;
 use serde::{Deserialize, Serialize};
 use serde_json::{from_value, json};
 use serde_json::Value;
 use std::{collections::HashMap, fs::File, path::Path};
 use tokio::time;
 use std::time::Instant;
-
-type Hjson = HashMap<String, Value>;
+use data_encoding::HEXUPPER;
+use ring::digest::{Context, Digest, SHA256};
+use std::io::{BufReader, Read};
 
 const URL: &str = "https://sheets.googleapis.com/v4/spreadsheets/1RIcSiQqPCw-6H55QIYwblIQDPpFQmDNC73ukFa05J7c:getByDataFilter";
 const URL_REFRESH: &str = "https://oauth2.googleapis.com/token";
+
+type Hjson = HashMap<String, Value>;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct DataSheets {
     title: String,
     row_data: Vec<Vec<String>>,
+}
+
+fn sha256_digest<R: Read>(mut reader: R) -> Result<Digest, Box<dyn std::error::Error>> {
+    let mut context = Context::new(&SHA256);
+    let mut buffer = [0; 1024];
+
+    loop {
+        let count = reader.read(&mut buffer)?;
+        if count == 0 {
+            break;
+        }
+        context.update(&buffer[..count]);
+    }
+
+    Ok(context.finish())
 }
 
 fn get_value<T: DeserializeOwned>(json: &Value, fallback: T) -> T {
@@ -50,48 +69,81 @@ pub async fn fetch_data(sheet_id: u64, access_token: &str) -> Result<Option<Data
     println!("{}", "fetching data...");
     let now = Instant::now();
     let client = reqwest::Client::new();
-    let resp = client.post(URL).bearer_auth(access_token).json(&req_body).send().await?.json::<Hjson>().await?;
+    let resp = client.post(URL).bearer_auth(access_token).json(&req_body).send().await?.text().await?;
     let elapsed = now.elapsed();
     println!("finished {:#?}", elapsed);
 
-    let sheets_json = resp.get("sheets").unwrap();
-    let sheets: Vec<Value> = get_value(&sheets_json, Vec::new());
+    let reader = BufReader::new(resp.as_bytes());
+    let digest = sha256_digest(reader)?;
+    let sha = HEXUPPER.encode(digest.as_ref());
 
-    println!("{}", "parsing data...");
-    let now = Instant::now();
-    let data: Vec<DataSheets> = sheets
-        .into_iter()
-        .map(|sheet| {
-            let row_data_json: Vec<Value> = get_value(&sheet["data"][0]["rowData"], Vec::new());
-            let title: String = get_value(&sheet["properties"]["title"], String::from(""));
+    let state_path = concat!(env!("CARGO_MANIFEST_DIR"), "/state.json");
+    let file = File::open(state_path);
+    let mut s: Map<String, Value> = match file {
+        Ok(f) => {
+            let reader = BufReader::new(f);
+            if let Ok(val) = serde_json::from_reader(reader) {
+                val
+            } else {
+                Map::new()
+            }
+        }
+        Err(e) => {
+            println!("{}", e);
+            let map = Map::new();
+            map
+        }
+    };
 
-            (title, row_data_json)
-        })
-        .map(|(title, row_data_json)| {
-            let row_data: Vec<Vec<String>> = row_data_json
-                .into_iter()
-                .map(|x| get_value(&x["values"], Vec::new()))
-                .map(|x: Vec<Value>| {
-                    let formatted_value: Vec<String> = x
-                        .into_iter()
-                        .map(|value| get_value(&value["formattedValue"], String::from("")))
-                        .collect();
-                    formatted_value
-                })
-                .filter(|val| !val.into_iter().all(|x| x.is_empty()))
-                .collect();
+    if let None = s.get(&sheet_id.to_string()) {
+        println!("insert {}: {}", sheet_id, sha);
+        s.insert(sheet_id.to_string(), Value::String(sha));
+        let file = File::create(state_path)?;
+        serde_json::to_writer_pretty(file, &s)?;
 
-            DataSheets { title, row_data }
-        })
-        .collect();
-    let elapsed = now.elapsed();
-    println!("finished {:#?}", elapsed);
+        let sheets_json: Value = serde_json::from_str(&resp).unwrap();
+        let sheets: Vec<Value> = get_value(&sheets_json["sheets"], Vec::new());
 
-    if data.len() > 0 {
-        Ok(Some(data[0].clone()))
+        println!("{}", "parsing data...");
+        let now = Instant::now();
+        let data: Vec<DataSheets> = sheets
+            .into_iter()
+            .map(|sheet| {
+                let row_data_json: Vec<Value> = get_value(&sheet["data"][0]["rowData"], Vec::new());
+                let title: String = get_value(&sheet["properties"]["title"], String::from(""));
+
+                (title, row_data_json)
+            })
+            .map(|(title, row_data_json)| {
+                let row_data: Vec<Vec<String>> = row_data_json
+                    .into_iter()
+                    .map(|x| get_value(&x["values"], Vec::new()))
+                    .map(|x: Vec<Value>| {
+                        let formatted_value: Vec<String> = x
+                            .into_iter()
+                            .map(|value| get_value(&value["formattedValue"], String::from("")))
+                            .collect();
+                        formatted_value
+                    })
+                    .filter(|val| !val.into_iter().all(|x| x.is_empty()))
+                    .collect();
+
+                DataSheets { title, row_data }
+            })
+            .collect();
+        let elapsed = now.elapsed();
+        println!("finished {:#?}", elapsed);
+
+        if data.len() > 0 {
+            Ok(Some(data[0].clone()))
+        } else {
+            Ok(None)
+        }
     } else {
+        println!("skip {}: {}", sheet_id, sha);
         Ok(None)
     }
+
 }
 
 async fn get_sheets(access_token: &str) -> Result<Vec<(u64, String)>, Box<dyn std::error::Error>> {
