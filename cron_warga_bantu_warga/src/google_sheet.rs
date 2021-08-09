04ea41f5f8be8b295b4e6a38c3951202319ec322
akane10 <targaryen.akane@gmail.com>
2021-08-10 00:38:45 +0700

    mod google_sheet

---
+++ cron_warga_bantu_warga/src/google_sheet.rs
@@ -0,0 +1,111 @@
+use crate::error::Error;
+use crate::get_value;
+use reqwest::header::HeaderName;
+use serde_json::from_value;
+use serde_json::json;
+use serde_json::Value;
+use std::collections::HashMap;
+use std::time::Instant;
+
+const URL: &str = "https://sheets.googleapis.com/v4/spreadsheets/1RIcSiQqPCw-6H55QIYwblIQDPpFQmDNC73ukFa05J7c:getByDataFilter";
+const URL_REFRESH: &str = "https://oauth2.googleapis.com/token";
+
+type Hjson = HashMap<String, Value>;
+
+pub async fn refresh_token() -> Result<String, Error> {
+    let refresh_token: &str = &dotenv::var("REFRESH_TOKEN").expect("Missing refresh token");
+    let client_id: &str = &dotenv::var("CLIENT_ID").expect("Missing client id");
+    let client_secret: &str = &dotenv::var("CLIENT_SECRET").expect("Missing client secret");
+    let url = format!(
+        "{}?client_id={}&client_secret={}&refresh_token={}&grant_type=refresh_token",
+        URL_REFRESH, client_id, client_secret, refresh_token
+    );
+
+    let hdr = HeaderName::from_static("content-length");
+    let client = reqwest::Client::new();
+    let resp = client
+        .post(url)
+        .header(hdr, "0")
+        .send()
+        .await?
+        .json::<Hjson>()
+        .await?;
+
+    if let Some(token) = resp.get("access_token") {
+        Ok(token.to_string())
+    } else {
+        println!("{:#?}", resp);
+        Err(Error::Others("failed to get access_token".to_string()))
+    }
+}
+
+pub async fn get_sheets(access_token: &str) -> Result<HashMap<u64, String>, Error> {
+    let hdr = HeaderName::from_static("content-length");
+    let client = reqwest::Client::new();
+    let resp = client
+        .post(URL)
+        .header(hdr, "0")
+        .bearer_auth(access_token)
+        .send()
+        .await?
+        .error_for_status()?
+        .json::<Hjson>()
+        .await?;
+
+    if let Some(sheets_json) = resp.get("sheets") {
+        let sheets: Vec<Value> = get_value(&sheets_json, Vec::new());
+        let mut data = HashMap::new();
+
+        for sheet in sheets[1..].iter() {
+            let id = from_value(sheet["properties"]["sheetId"].clone());
+            let title = from_value(sheet["properties"]["title"].clone());
+
+            match (id, title) {
+                (Ok(id), Ok(title)) => {
+                    data.insert(id, title);
+                }
+                _ => (),
+            }
+        }
+
+        Ok(data)
+    } else {
+        println!("{:#?}", resp);
+        Err(Error::Others("failed to get sheets".to_string()))
+    }
+}
+
+pub async fn get_sheet_data(sheet_id: u64, access_token: &str) -> Result<Vec<Value>, Error> {
+    let req_body = json!({
+      "includeGridData": true,
+      "dataFilters": [
+        {
+          "gridRange": {
+            "sheetId": sheet_id
+          }
+        }
+      ]
+    });
+
+    println!("{}", "fetching data...");
+    let now = Instant::now();
+    let client = reqwest::Client::new();
+    let resp = client
+        .post(URL)
+        .bearer_auth(access_token)
+        .json(&req_body)
+        .send()
+        .await?
+        .json::<Hjson>()
+        .await?;
+    let elapsed = now.elapsed();
+    println!("finished {:#?}", elapsed);
+
+    if let Some(val) = resp.get("sheets") {
+        let sheets: Vec<Value> = from_value(val.clone())?;
+        Ok(sheets)
+    } else {
+        println!("{:#?}", resp);
+        Err(Error::Others("failed to get sheets".to_string()))
+    }
+}
--- cron_warga_bantu_warga/src/main.rs
+++ cron_warga_bantu_warga/src/main.rs
@@ -1,7 +1,9 @@
 use chrono::prelude::*;
 use data_encoding::HEXUPPER;
 use error::Error;
-use reqwest::header::HeaderName;
+use google_sheet::get_sheet_data;
+use google_sheet::get_sheets;
+use google_sheet::refresh_token;
 use ring::digest::Context;
 use ring::digest::Digest;
 use ring::digest::SHA256;
@@ -20,11 +22,7 @@ use std::time::Instant;
 use tokio::time;
 
 mod error;
-
-const URL: &str = "https://sheets.googleapis.com/v4/spreadsheets/1RIcSiQqPCw-6H55QIYwblIQDPpFQmDNC73ukFa05J7c:getByDataFilter";
-const URL_REFRESH: &str = "https://oauth2.googleapis.com/token";
-
-type Hjson = HashMap<String, Value>;
+mod google_sheet;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct DataSheets {
@@ -49,35 +47,8 @@ fn sha256_digest<R: Read>(mut reader: R) -> Result<Digest, Error> {
     Ok(context.finish())
 }
 
-fn get_value<T: DeserializeOwned>(json: &Value, fallback: T) -> T {
+pub fn get_value<T: DeserializeOwned>(json: &Value, fallback: T) -> T {
     from_value(json.clone()).unwrap_or(fallback)
-}
-
-async fn refresh_token() -> Result<String, Error> {
-    let refresh_token: &str = &dotenv::var("REFRESH_TOKEN").expect("Missing refresh token");
-    let client_id: &str = &dotenv::var("CLIENT_ID").expect("Missing client id");
-    let client_secret: &str = &dotenv::var("CLIENT_SECRET").expect("Missing client secret");
-    let url = format!(
-        "{}?client_id={}&client_secret={}&refresh_token={}&grant_type=refresh_token",
-        URL_REFRESH, client_id, client_secret, refresh_token
-    );
-
-    let hdr = HeaderName::from_static("content-length");
-    let client = reqwest::Client::new();
-    let resp = client
-        .post(url)
-        .header(hdr, "0")
-        .send()
-        .await?
-        .json::<Hjson>()
-        .await?;
-
-    if let Some(token) = resp.get("access_token") {
-        Ok(token.to_string())
-    } else {
-        println!("{:#?}", resp);
-        Err(Error::Others("failed to get access_token".to_string()))
-    }
 }
 
 fn parse_data(sheet: &Value) -> Result<DataSheets, Error> {
@@ -107,142 +78,81 @@ fn parse_data(sheet: &Value) -> Result<DataSheets, Error> {
     Ok(data)
 }
 
-pub async fn fetch_data(
-    sheet_id: u64,
-    title: &str,
-    access_token: &str,
-) -> Result<Option<DataSheets>, Error> {
-    let req_body = json!({
-      "includeGridData": true,
-      "dataFilters": [
-        {
-          "gridRange": {
-            "sheetId": sheet_id
-          }
-        }
-      ]
-    });
+fn compare_hash(
+    data: &Vec<Value>,
+    data_json: Option<&Value>,
+) -> Result<(bool, bool, String), Error> {
+    let sheets_str: String = serde_json::to_string(&data)?;
 
-    println!("{}", "fetching data...");
-    let now = Instant::now();
-    let client = reqwest::Client::new();
-    let resp = client
-        .post(URL)
-        .bearer_auth(access_token)
-        .json(&req_body)
-        .send()
-        .await?
-        .json::<Hjson>()
-        .await?;
-    let elapsed = now.elapsed();
-    println!("finished {:#?}", elapsed);
+    let reader = BufReader::new(sheets_str.as_bytes());
+    let digest = sha256_digest(reader)?;
+    let sha: String = HEXUPPER.encode(digest.as_ref());
 
-    let sheets: Vec<Value> = get_value(&resp["sheets"], Vec::new());
-
-    if sheets.len() <= 0 {
-        Ok(None)
-    } else {
-        let sheets_str: String = serde_json::to_string(&resp["sheets"])?;
-
-        let reader = BufReader::new(sheets_str.as_bytes());
-        let digest = sha256_digest(reader)?;
-        let sha: String = HEXUPPER.encode(digest.as_ref());
-
-        let p = Path::new(env!("CARGO_MANIFEST_DIR"))
-            .join("..")
-            .join("public/data/data.json");
-        let file = File::open(&p);
-        let mut s: HashMap<String, Value> = match file {
-            Ok(f) => {
-                let reader = BufReader::new(f);
-                serde_json::from_reader(reader).unwrap_or(HashMap::new())
-            }
-            Err(e) => {
-                println!("{}", e);
-                HashMap::new()
-            }
-        };
-
-        let mut data_json = s.get_mut(&sheet_id.to_string());
-
-        let (is, is_new) = match data_json {
-            Some(ref val) => {
-                if sha.ne(&val["hash"]) {
-                    (true, false)
-                } else {
-                    (false, false)
-                }
-            }
-            _ => (true, true),
-        };
-        if is {
-            println!("{}", "parsing data...");
-            let now = Instant::now();
-            let data = parse_data(&sheets[0])?;
-            let elapsed = now.elapsed();
-            println!("finished {:#?}", elapsed);
-
-            let total_row = data.row_data.len();
-
-            if is_new {
-                println!("INSERT NEW {} - {}", title, sha);
-                let val = json!({ "title": title,  "total_row": total_row, "hash": Value::String(sha.clone()) });
-                s.insert(sheet_id.to_string(), val.clone());
-                let file = File::create(p)?;
-                serde_json::to_writer(file, &s)?;
+    let (is_not_match, is_new) = match data_json {
+        Some(ref val) => {
+            if sha.ne(&val["hash"]) {
+                (true, false)
             } else {
-                match data_json {
-                    Some(ref mut val) => {
-                        println!("NEW HASH {} - {}", title, sha);
-                        val["hash"] = Value::String(sha.clone());
-                        val["total_row"] = json!(total_row);
-                        let file = File::create(p)?;
-                        serde_json::to_writer(file, &s)?;
-                    }
-                    _ => println!("should never happens"),
-                }
+                (false, false)
             }
-            Ok(Some(data))
-        } else {
-            println!("SKIP {}: {}", title, sha);
-            Ok(None)
         }
-    }
+        _ => (true, true),
+    };
+
+    Ok((is_not_match, is_new, sha))
 }
 
-async fn get_sheets(access_token: &str) -> Result<HashMap<u64, String>, Error> {
-    let hdr = HeaderName::from_static("content-length");
-    let client = reqwest::Client::new();
-    let resp = client
-        .post(URL)
-        .header(hdr, "0")
-        .bearer_auth(access_token)
-        .send()
-        .await?
-        .error_for_status()?
-        .json::<Hjson>()
-        .await?;
+fn get_data(data: Vec<Value>, sheet_id: u64, title: &str) -> Result<Option<DataSheets>, Error> {
+    let p = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("..")
+        .join("public/data/data.json");
+    let file = File::open(&p);
+    let mut s: HashMap<String, Value> = match file {
+        Ok(f) => {
+            let reader = BufReader::new(f);
+            serde_json::from_reader(reader).unwrap_or(HashMap::new())
+        }
+        Err(e) => {
+            println!("{}", e);
+            HashMap::new()
+        }
+    };
 
-    if let Some(sheets_json) = resp.get("sheets") {
-        let sheets: Vec<Value> = get_value(&sheets_json, Vec::new());
-        let mut data = HashMap::new();
+    let mut data_json = s.get_mut(&sheet_id.to_string());
 
-        for sheet in sheets[1..].iter() {
-            let id = from_value(sheet["properties"]["sheetId"].clone());
-            let title = from_value(sheet["properties"]["title"].clone());
+    let (is_not_match, is_new, sha) = compare_hash(&data, data_json.as_deref())?;
 
-            match (id, title) {
-                (Ok(id), Ok(title)) => {
-                    data.insert(id, title);
+    if is_not_match {
+        println!("{}", "parsing data...");
+        let now = Instant::now();
+        let data = parse_data(&data[0])?;
+        let elapsed = now.elapsed();
+        println!("finished {:#?}", elapsed);
+
+        let total_row = data.row_data.len();
+
+        if is_new {
+            println!("INSERT NEW {} - {}", title, sha);
+            let val = json!({ "title": title,  "total_row": total_row, "hash": Value::String(sha.clone()) });
+            s.insert(sheet_id.to_string(), val);
+            let file = File::create(p)?;
+            serde_json::to_writer(file, &s)?;
+        } else {
+            match data_json {
+                Some(ref mut val) => {
+                    println!("NEW HASH {} - {}", title, sha);
+                    val["hash"] = Value::String(sha);
+                    val["total_row"] = json!(total_row);
+                    let file = File::create(p)?;
+                    serde_json::to_writer(file, &s)?;
                 }
-                _ => (),
+                _ => println!("should never happens"),
             }
         }
-
-        Ok(data)
+        Ok(Some(data))
     } else {
-        println!("{:#?}", resp);
-        Err(Error::Others("failed to get sheets".to_string()))
+        println!("SKIP {}: {}", title, sha);
+        Ok(None)
     }
 }
 
@@ -253,22 +163,24 @@ async fn run(access_token: &str) -> Result<(), Error> {
         .join("public/data");
 
     for (id, title) in sheet_ids.iter() {
-        if let Some(data) = fetch_data(*id, title, &access_token).await? {
-            println!("{}", "writing file...");
-            let now = Instant::now();
-            let filename = format!("{}/{}.json", p.to_str().unwrap(), id);
-            let filename_lastest = format!("{}/last_updated.json", p.to_str().unwrap());
-            let file = File::create(filename)?;
-            let file_lastest = File::create(filename_lastest)?;
-            serde_json::to_writer(file, &data)?;
+        if let Ok(val) = get_sheet_data(*id, &access_token).await {
+            if let Some(data) = get_data(val, *id, title)? {
+                println!("{}", "writing file...");
+                let now = Instant::now();
+                let filename = format!("{}/{}.json", p.to_str().unwrap(), id);
+                let filename_lastest = format!("{}/last_updated.json", p.to_str().unwrap());
+                let file = File::create(filename)?;
+                let file_lastest = File::create(filename_lastest)?;
+                serde_json::to_writer(file, &data)?;
 
-            let last = json!({ "sheet_id": data.sheet_id, "title": data.title, "updated_at": data.updated_at });
-            serde_json::to_writer(file_lastest, &last)?;
+                let last = json!({ "sheet_id": data.sheet_id, "title": data.title, "updated_at": data.updated_at });
+                serde_json::to_writer(file_lastest, &last)?;
 
-            let elapsed = now.elapsed();
-            println!("finished {:#?}", elapsed);
-        } else {
-            println!("no data");
+                let elapsed = now.elapsed();
+                println!("finished {:#?}", elapsed);
+            } else {
+                println!("no data");
+            }
         }
     }
 
